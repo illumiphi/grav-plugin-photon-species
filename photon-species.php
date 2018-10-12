b60c0bfeec1715d88b6370a973acdb7bafa3c89c
i-am-phi <phi@i-am-phi.net>
2018-10-11 17:28:38 -0700

    built in css js config

--- photon-species.php
+++ photon-species.php
@@ -86,7 +86,7 @@ class PhotonSpeciesPlugin extends Plugin
         // styles
         if ($this->config->get('plugins.photon-species.built_in_css')) {
           $css = 'plugin://photon-species/assets/species.css';
-          // $assets->addCss($css);
+          // $assets->addCss($css, 100, false, 'photon-plugins' );
           $assets->addCss($css, 100, false, 'photon-plugins' );
 
         }
@@ -94,6 +94,46 @@ class PhotonSpeciesPlugin extends Plugin
         // scripts
         if ($this->config->get('plugins.photon-species.built_in_js')) {
           $js = 'plugin://photon-species/assets/species.js';
+          $assets->addJs($js, 100, false, 'defer', 'photon-plugins' );
+        }
+
+
+      }
+    }
+      if ($page->template() == 'family')
+      {
+
+        // styles
+        if ($this->config->get('plugins.photon-species.built_in_css')) {
+          $css = 'plugin://photon-species/assets/family.css';
+          // $assets->addCss($css, 100, false, 'photon-plugins' );
+          $assets->addCss($css, 100, false, 'photon-plugins' );
+
+        }
+
+        // scripts
+        if ($this->config->get('plugins.photon-species.built_in_js')) {
+          $js = 'plugin://photon-species/assets/family.js';
+          $assets->addJs($js, 100, false, 'defer', 'photon-plugins' );
+        }
+
+
+      }
+    }
+      if ($page->template() == 'genus')
+      {
+
+        // styles
+        if ($this->config->get('plugins.photon-species.built_in_css')) {
+          $css = 'plugin://photon-species/assets/genus.css';
+          // $assets->addCss($css, 100, false, 'photon-plugins' );
+          $assets->addCss($css, 100, false, 'photon-plugins' );
+
+        }
+
+        // scripts
+        if ($this->config->get('plugins.photon-species.built_in_js')) {
+          $js = 'plugin://photon-species/assets/genus.js';
           $assets->addJs($js, 100, false, 'defer', 'photon-plugins' );
         }
 
